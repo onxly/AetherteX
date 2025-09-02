0ee01425f76f3221e3a970c3cb2213241cdc8ee7
Munchie-afk <manca.otlotleng@gmail.com>
2025-09-02 18:45:22 +0200

    adds Cart, login and register pages

--- React/aethertex/src/pages/login.jsx
+++ React/aethertex/src/pages/login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
+import Zeus from './Zeus.jpg';  // <-- import the image
+import '../styling/login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Login (pages/login.jsx)</div>;
+  return (
+<div className="formStyle">
+  
+    <div><img src={Zeus} width={300} height={300} alt="Zeus" /></div>
+
+         
+       <div style={{marginRight:100}}><input type="text" id="email" placeholder="Member email" className="Boxs" /> </div> 
+       <div style={{marginRight:100}}><input type="password" id="password" placeholder="Password" className="Boxs"/></div>
+
+      <button type="submit" className="loginbtn">Log in</button>
+      <p>Not a member? <Link to='/register'>Register here!</Link></p>
+  
+    
+</div>
+
+  );
 }
 
 export default Login;
--- React/aethertex/src/pages/register.jsx
+++ React/aethertex/src/pages/register.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+    import React from 'react';
+    import '../styling/register.css';
+    import { Link } from 'react-router-dom';
 
-function Register() {
-  return <div>Register (pages/register.jsx)</div>;
-}
+    function Register() {
+    return (
+        <form style={{backgroundColor:'black'}}>
+        <div className="column">
+            
+            <h2>Create your Account</h2>
 
-export default Register;
+            <label htmlFor="username" style={{ marginRight: "235px" }}>Username</label>
+            <input type="text" id="username" placeholder="Type your Username" className="RegisterBoxs" style={{ height: "60px" }} />
+
+            <label htmlFor="name" style={{ marginRight: "250px" }}>Name</label>
+            <input type="text" id="name" placeholder="Type your Name" className="RegisterBoxs" />
+
+            <label htmlFor="surname" style={{ marginRight: "245px" }}>Surname</label>
+            <input type="text" id="surname" placeholder="Type your Surname" className="RegisterBoxs" />
+
+            <label htmlFor="email" style={{ marginRight: "260px" }}>Email</label>
+            <input type="text" id="email" placeholder="Type your Email" className="RegisterBoxs" />
+
+            <label htmlFor="phoneNumber" style={{ marginRight: "204px" }}>Phone Number</label>
+            <input type="text" id="phoneNumber" placeholder="Type your Phone Number" className="RegisterBoxs" />
+
+            <label htmlFor="password" style={{ marginRight: "240px" }}>Password</label>
+            <input type="password" id="password" placeholder="Type your Password" className="RegisterBoxs" />
+
+            <label htmlFor="confirmPassword" style={{ marginRight: "184px" }}>Confirm Password</label>
+            <input type="password" id="confirmPassword" placeholder="Confirm your Password" className="RegisterBoxs" />
+
+            <button type="submit" className="Registerloginbtn">Create Account</button>
+            <p style={{ marginTop: "10px" }}>
+                Already a Member? <Link to="/login">Click here!</Link>
+            </p>
+            <hr />
+        </div>
+        </form>
+    );
+    }
+
+    export default Register;
