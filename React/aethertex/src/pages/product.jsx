ac3e29c38ca2b259c663eb43fc37b4da0fdc4274
BoyznFem <fememokotedi@gmail.com>
2025-09-02 14:14:18 +0200

    Fixes

--- React/aethertex/src/pages/product.jsx
+++ React/aethertex/src/pages/product.jsx
@@ -43,6 +43,13 @@ function Product() {
         Benchmark: 49.6,
     };
 
+    let RAM = {
+      Name: "Corsair Vengeance RGB PRO",
+      Speed: 3200,
+      Channel: "Single channel",
+      Benchmark: 88.5,
+    };
+
       let CusReviews = [
               { user: "Boyzn1", rating: 5, comment: "Excellent product!" },
               { user: "Boyzn2", rating: 4, comment: "Very good, but could be improved." },
@@ -67,8 +74,9 @@ function Product() {
                 specs={specs}
                 CPU={CPU}
                 GPU={GPU}
+                RAM={RAM}
             />
-            <PurchaseProduct 
+  -          <PurchaseProduct 
                 price={price}
                 stock={stock}
             />
