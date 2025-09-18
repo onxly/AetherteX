1cafe2883e8aa4c1bfaf9201da4891580184e0a0
BMCHIROWAMHANGU <bmmunashe@gmail.com>
2025-09-18 19:27:03 +0200

    to pull from main

--- React/aethertex/src/components/AddressBox.jsx
+++ React/aethertex/src/components/AddressBox.jsx
@@ -1,17 +1,20 @@
-import "../stylesheets/AddressBox.css"
+import "../stylesheets/AddressBox.css";
 
-function AddressBox(addr){
+export function AddressBox(addr){
+    
     return(
     <div className="AddrBox">
-        <div className="AddrDetails">
+        <div>
             <u>{addr.Name}</u>
             <address>{addr.Street}, {addr.City}, {addr.Postal}</address>
             <em>{addr.Phone}</em>
+            <p>{animes[0].title}</p>
         </div>
         <div className="Addrbuttons">
             <button className="editadress">Edit</button>
             <button className="editadress">Remove</button>
             <button className="editadress">Set as default</button>
+            <button className="getanime" onClick={async ()=> await getAnimes()}>Get Animes</button>
         </div>                          
     </div>
     );
--- React/aethertex/src/jsfunctions/alljsfunctions.js
+++ React/aethertex/src/jsfunctions/alljsfunctions.js
@@ -1,4 +1,91 @@
 import axios from "axios";
 
+async function getAdress()
+{
+    const res=await axios.get();
+    return res.data;
+}
 
+async function getAdmin()
+{
+    const res=await axios.get();
+    return res.data;
+}
 
+async function getCart()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getUser()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getClient()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getCpu()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getGPU()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getInvoice()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getProduct()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getPurchase()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getRam()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getRating()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getShipping()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getStorage()
+{
+    const res=await axios.get();
+    return res.data;
+}
+
+async function getOrders()
+{
+    const res=await axios.get();
+    return res.data;
+}
