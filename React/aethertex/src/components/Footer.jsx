99086833f38b723987055808badc8a4c7e454478
BMCHIROWAMHANGU <bmmunashe@gmail.com>
2025-09-24 00:15:13 +0200

    nuke

--- React/aethertex/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "../stylesheets/footer.css";
-function Footer(){
-    const Year = new Date().getFullYear();
-    return (
-        <footer>
-            <span>Copyright &copy; 2025-{Year} AetherteX | All Rights Reserved</span>
-        </footer>
-    )
-} export default Footer
