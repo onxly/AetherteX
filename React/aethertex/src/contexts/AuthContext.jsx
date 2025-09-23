44e1976a4d5f385da367e50d4082bf907627c05c
onxly <itu.m403@gmail.com>
2025-09-23 21:41:24 +0200

    fixed register error

--- React/aethertex/src/contexts/AuthContext.jsx
+++ React/aethertex/src/contexts/AuthContext.jsx
@@ -54,7 +54,88 @@ export function AuthProvider({ children }) {
     }
   }
 
-  // Keep your other functions (register, addCart, etc.) here as before
+  /** *Registers a user *
+   * * @param {String} name Name of user
+   * * @param {String} surname Last name of user *
+   * @param {String} username Preffered name of user *
+   * @param {String} email Email address of user *
+   * @param {String} phoneNumber Phone Number of user *
+   * @param {String} password Password of user */
+
+  async function VerifyAdmin(UserId, AdminCode) {
+    const reqdata = { userId: UserId, adminCode: AdminCode };
+    const res = await axios.post(apilink + "users/verifyadmin", reqdata);
+  }
+
+  async function register(Name, Surname, Email, PhoneNumber, Password) {
+    const reqdata = {
+      name: Name,
+      surname: Surname,
+      email: Email,
+      phoneNumber: PhoneNumber,
+      password: await hashPassword(Password),
+    };
+    try {
+      const res = await axios.post(apilink + "users/register", reqdata);
+    } catch (ex) {
+      console.log("ERROR: <REGISTER>: " + JSON.stringify(ex));
+    }
+  }
+
+  async function updateUser(id, Name, Email) {
+    const reqdata = { name: Name, email: Email };
+    const res = await axios.put(apilink + "users/update" + id, reqdata);
+  }
+
+  async function changePassword(UserId, OldPassword, NewPassword) {
+    const reqdata = {
+      userId: UserId,
+      oldPassword: OldPassword,
+      newPassword: NewPassword,
+    };
+    const res = await axios.put(apilink + "users/changepassword", reqdata);
+  }
+
+  async function updateLoyaltyPoints(ClientId, PointsToAdd) {
+    const reqdata = { clientId: ClientId, pointsToAdd: PointsToAdd };
+    const res = await axios.put(apilink + "users/loyaltypoints", reqdata);
+  }
+  async function getLoyaltyPoints(id) {
+    const res = await axios.get(apilink + "users/loyaltypoints" + id, reqdata);
+  }
+
+  async function updatePremiumStatus(ClientId, IsPremium) {
+    const reqdata = { clientId: ClientId, isPremium: IsPremium };
+    const res = await axios.put(apilink + "users/premiumstatus" + id, reqdata);
+  }
+
+  function addCart(newid, newImg, newTitle, newPrice, newStock, newQuantity) {
+    setCart((prevCart) => {
+      const existing = prevCart.find((item) => item.id === newid);
+      if (existing) {
+        return prevCart.map((item) =>
+          item.id === newid
+            ? { ...item, Quantity: item.Quantity + newQuantity }
+            : item
+        );
+      } else {
+        return [
+          ...prevCart,
+          {
+            id: newid,
+            Img: newImg,
+            Title: newTitle,
+            Price: newPrice,
+            Stock: newStock,
+            Quantity: newQuantity,
+          },
+        ];
+      }
+    });
+    {
+      /*} const ckCart = cart.find(c => c.id === newid); if (ckCart == null) { setCart(prev => [ ...prev, { id: newid, Img: newImg, Title: newTitle, Price: newPrice, Stock: newStock, Quantity: newQuantity } ]); } */
+    }
+  }
 
   return (
     <AuthContext.Provider
@@ -66,6 +147,7 @@ export function AuthProvider({ children }) {
         setCart,
         login,
         logout,
+        register,
       }}
     >
       {children}
