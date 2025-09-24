480b6a604cad17f426a50936a59a745253e611a4
onxly <itu.m403@gmail.com>
2025-09-24 15:47:59 +0200

    cart context fix

--- React/aethertex/src/contexts/CartContext.jsx
+++ React/aethertex/src/contexts/CartContext.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { createContext, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import axios from "axios";
